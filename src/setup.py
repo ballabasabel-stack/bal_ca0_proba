98fb09cc51ee030c750b9f6f52eeb187e4f6f04b
ballabasabel-stack <ballabasabel@gmail.com>
2025-10-08 13:02:42 +0200

    6

--- src/setup.py
+++ src/setup.py
@@ -4,23 +4,25 @@ package_name = 'bal_ca0_proba'
 
 setup(
     name=package_name,
-    version='0.0.2',
+    version='0.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['bal_ca0_proba/launch_example1.launch.py']),
+        ('share/' + package_name + '/launch', ['launch/launch_example1.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Te',
-    maintainer_email='te@pelda.hu',
-    description='Szinusz és koszinusz jel generáló ROS2 node',
+    maintainer='Your Name',
+    maintainer_email='you@example.com',
+    description='Publishes sine and cosine waves for visualization in rqt_plot',
     license='MIT',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'trig_wave_publisher = bal_ca0_proba.trig_wave_publisher:main',
+            'trig_publisher = bal_ca0_proba.trig_publisher:main',
+            'trig_subscriber = bal_ca0_proba.trig_subscriber:main',
         ],
     },
 )
