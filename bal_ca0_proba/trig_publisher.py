b88256cb4752547862d148708c83579462b47350
ballabasabel-stack <ballabasabel@gmail.com>
2025-10-09 15:05:08 +0200

    18

--- bal_ca0_proba/trig_publisher.py
+++ bal_ca0_proba/trig_publisher.py
@@ -27,3 +27,6 @@ def main(args=None):
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
+if _name_ == '_main_':
+    main()
--- bal_ca0_proba/trig_subscriber.py
+++ bal_ca0_proba/trig_subscriber.py
@@ -20,3 +20,6 @@ def main(args=None):
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
+if _name_ == '_main_':
+    main()
