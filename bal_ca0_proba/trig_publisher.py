4356bacf3122ed79d3a2ba6017c2de2a4a3583e4
ballabasabel-stack <ballabasabel@gmail.com>
2025-10-09 16:16:47 +0200

    23

--- bal_ca0_proba/trig_publisher.py
+++ bal_ca0_proba/trig_publisher.py
@@ -6,7 +6,7 @@ import math
 class TrigPublisher(Node):
     def __init__(self):
         super().__init__('trig_publisher')
-        self.publisher_sin = self.create_publisher(Float32, 'sin_wave', 10)
+        self.publisher_sin = self.create_publisher(Float32, 'sin_wave', 100)
         self.publisher_cos = self.create_publisher(Float32, 'cos_wave', 10)
         self.timer = self.create_timer(0.1, self.timer_callback)
         self.t = 0.0
